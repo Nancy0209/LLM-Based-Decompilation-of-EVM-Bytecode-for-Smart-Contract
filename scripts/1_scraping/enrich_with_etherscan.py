b8d3cd5e2fae96831224ac7b4efa4309a84e0834
Nancy0209 <94922452+Nancy0209@users.noreply.github.com>
2025-09-05 22:28:12 +0100

    Update enrich_with_etherscan.py

--- scripts/1_scraping/enrich_with_etherscan.py
+++ scripts/1_scraping/enrich_with_etherscan.py
@@ -31,7 +31,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('LOG_FILE, encoding="utf-8"'), # 
+        logging.FileHandler(LOG_FILE, encoding="utf-8"), # 
         logging.StreamHandler(sys.stdout) # 
     ]
 )
